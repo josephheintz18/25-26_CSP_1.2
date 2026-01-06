8c25f0f214584edb02ab134d7fcacaf7620439f4
JHeintz <Joseph.Heintz@vtsd.com>
2026-01-06 11:25:41 -0500

    cool beans

--- 1.1.3/appleavalanche.py
+++ 1.1.3/appleavalanche.py
@@ -1,40 +1,37 @@
-#   a123_apple_1.py
 import turtle as trtl
 
-#-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -800
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
+wn.bgpic("background.gif")
 apple = trtl.Turtle()
-drawer = trtl.Turtle()
-ground_height= (-200,0)
-drawer.hideturtle()
+apple.penup()
 
-#-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
-def draw_an_A():
-  drawer.color("violet")
-  drawer.write("A", font=("Arial", 74, "bold"))
-
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  wn.update()
+  wn.tracer(False)
+  draw_letter("A", active_apple)
+  wn.tracer(True)
 
 def drop_apple():
-    apple.goto(apple.xcor(), ground_height)
+  apple.goto(apple.xcor(), ground_height)
 
-#-----function calls-----
-wn.onkeypress(draw_an_A, "a")
-
-if wn.onkeypress(draw_an_A, "a"):
-    apple.right(90)
-    apple.forward(90000)
-
-wn.listen()
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 74, "bold"))
+  active_apple.setpos(remember_position)
 
 draw_apple(apple)
+wn.onkeypress(drop_apple, "a")
 
-wn.mainloop()
+wn.listen()
+trtl.mainloop()
